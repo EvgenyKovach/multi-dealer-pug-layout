6c152a1820ca91fc7ff9a1053f678fa75d438632
kovach <zheka.ae@mail.ru>
2020-12-22 18:56:50 +0500

    change gitignore

--- app/site/src/js/app.js
+++ app/site/src/js/app.js
@@ -1,1 +1,4 @@
-import "../styles/app.scss"
+import '../styles/app.scss';
+import './popup';
+
+let yuy = document.location.href;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,82 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs = require('fs');
+
+const PATHS = {
+    src: path.join(__dirname, './app/site/src'),
+    dist: path.join(__dirname, './app/site/dist'),
+};
+
+const PAGES_DIR = `${PATHS.src}/template/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
+    entry: {
+        app: path.resolve(__dirname, './app/site/src/js/app.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './app/site/dist'),
+        filename: './js/[name].js',
+    },
 
-	entry: {
-		app: path.resolve(__dirname, './app/site/src/js/app.js'),
-	},
-	output: {
-		path: path.resolve(__dirname, './app/site/dist/js'),
-		filename: '[name].js',
-	},
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                oneOf: [
+                    // this applies to pug imports inside JavaScript
+                    {
+                        use: ['pug-loader'],
+                    },
+                ],
+            },
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            // CSS, PostCSS, Sass
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            }, // CSS, PostCSS, Sass
+        ],
+    },
 
-	module: {
-		rules: [
-			// JavaScript
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-				type: 'asset/inline',
-			},
-			// CSS, PostCSS, Sass
-			{
-				test: /\.(scss|css)$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-			},
-		],
-	},
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        compress: true,
+        writeToDisk: true,
+    },
 
-	plugins: [
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: '../css/[name].css'
-		})
-	],
+    plugins: [
+        new CleanWebpackPlugin(),
 
-}
+        // new HtmlWebpackPlugin({
+        //     template: './app/site/dist/index.html',
+        // }),
+
+        new MiniCssExtractPlugin({
+            filename: './css/[name].css',
+        }),
+
+        new webpack.HotModuleReplacementPlugin(),
+
+        ...PAGES.map(
+            page =>
+                new HtmlWebpackPlugin({
+                    template: `${PAGES_DIR}/${page}`,
+                    filename: `./template/${page.replace(/\.pug/, '.html')}`,
+                })
+        ),
+    ],
+};
