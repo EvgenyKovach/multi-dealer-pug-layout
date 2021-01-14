d670125a6c2300b2d41020dd41b5144570b1756e
kovach <zheka.ae@mail.ru>
2021-01-14 11:32:26 +0500

    add indexDescription.pug add moreQuestions.pug

--- app/site/src/js/app.js
+++ app/site/src/js/app.js
@@ -2,16 +2,16 @@
 import '../styles/app.scss';
 import 'swiper/swiper-bundle.css';
 
-
 // js
 import lazyLoad from './lazyLoad';
 import toggleMenu from './toggleMenu';
 import swiper from './swiper';
 import carCard from "./carCard";
-
+import hiddenTextDescription from "./hiddenTextDescription";
 
 // inits
 toggleMenu();
 swiper();
 carCard();
 lazyLoad();
+hiddenTextDescription();
---
+++ app/site/src/js/hiddenTextDescription.js
@@ -0,0 +1,13 @@
+export default () => {
+  const lockAllbtn = document.querySelector('.js-lock-all-btn')
+  const hiddenText = document.querySelector('.js-hidden-text')
+
+  lockAllbtn.onclick = () => {
+
+    hiddenText.classList.toggle('showText')
+    if (hiddenText.classList.contains('showText')) lockAllbtn.textContent = 'скрыть'
+    else lockAllbtn.textContent = 'открыть текст'
+
+  }
+
+}
