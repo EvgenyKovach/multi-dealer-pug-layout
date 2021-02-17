88b8527086e4b728a7008a808b5c51b9e2265287
kovach <zheka.ae@mail.ru>
2021-02-17 09:29:30 +0500

    rework card auto

--- app/site/src/js/app.js
+++ app/site/src/js/app.js
@@ -7,18 +7,17 @@ import '../styles/app.scss';
 
 
 // js
-
 import lazyLoad from './lazyLoad';
 import toggleMenu from './toggleMenu';                        // скрипт тагла мобильного меню
 import swiper from './swiper';                                // инит слайдера(ов)
 import carCard from './carCard';                              // инит слайдера в карточке + переработка pagination
 import hiddenTextDescription from './hiddenTextDescription';  // скрытый текст на главной
 import scrollTop from './scrollTop';                          // кнопка "вверх"
-import checkbox from "./checkbox-l";                            // настройка чекбоксов в фильтре
-import toggleFilters from "./toggleFilters";                  // тагл разделов фильтра
-import rangeSlider from "./rangeSlider";                      // инит ползунка слайдера
-import colorPickFilter from "./checkbox-color";              // настройка чекбокса выбора цвета фильтра
-import stickySidebar from "./stickySidebar";                  // инит плавающей настройки фильтра
+import checkbox from "./checkbox-l";                          //  настройка чекбоксов в фильтре
+import toggleFilters from "./toggleFilters";                   // тагл разделов фильтра
+import rangeSlider from "./rangeSlider";                       // инит ползунка слайдера
+import colorPickFilter from "./checkbox-color";                // настройка чекбокса выбора цвета фильтра
+import stickySidebar from "./stickySidebar";                   // инит плавающей настройки фильтра
 import select from './select';
 import mobileFilter from "./mobileFilter";                     // мобильный фильтр
 import footerBrands from "./footerBrands";                     // размер списка бренда в футере, вынужденный костыль
