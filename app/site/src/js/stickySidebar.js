9574c9a0f2f7bea7297e1bb4d7db6109bf63a5c6
kovach <zheka.ae@mail.ru>
2021-02-17 15:58:58 +0500

    add shadows/ popular models fix boxes

--- app/site/src/js/stickySidebar.js
+++ app/site/src/js/stickySidebar.js
@@ -2,7 +2,7 @@ import stickySidebar from 'sticky-sidebar-v2';
 
 export default () => {
 
-  if (window.innerWidth > 1085) {
+  if (window.innerWidth > 1085 && document.querySelector('#filter')) {
 
     const sidebar = new StickySidebar('#filter', {
       topSpacing: 25,
@@ -11,5 +11,6 @@ export default () => {
     });
 
   }
+
 
 }
