0d9e9263239e3c1a1829ead7c33ea3837d12b831
kovach <zheka.ae@mail.ru>
2021-02-22 11:53:38 +0500

    carinfo slider adaptive

--- app/site/src/js/thumbSlider.js
+++ app/site/src/js/thumbSlider.js
@@ -5,21 +5,29 @@ export default () => {
     freeMode: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
+    direction: 'vertical',
   });
 
   const galleryTop = new Swiper('.gallery-top', {
-    width: 693,
+    width: null,
     loop: true,
     centeredSlides: true,
     slidesPerView: 1,
     spaceBetween: 5,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
     },
     thumbs: {
       swiper: galleryThumbs
+    },
+    breakpoints: {
+      768: {
+        width: 693,
+      }
     }
   });
 
 }
+
+
